52e808506dd74e6ee7e50678caff846d2757487f
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-08-14 13:55:48 -0400

    moved DI and added MemoryCaching injection

--- SuggestionAppUI/RegisterServices.cs
+++ SuggestionAppUI/RegisterServices.cs
@@ -4,7 +4,9 @@ public static class RegisterServices
 {
     public static void ConfigureServices(this WebApplicationBuilder builder)
     {
-
-
+        // Add services to the container.
+        builder.Services.AddRazorPages();
+        builder.Services.AddServerSideBlazor();
+        builder.Services.AddMemoryCache();
     }
 }
