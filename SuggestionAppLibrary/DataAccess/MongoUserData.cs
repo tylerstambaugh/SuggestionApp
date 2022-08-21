1b90948cba246adcad374d547e7592c7b3a8361d
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-08-21 12:16:15 -0400

    setting up DB connection

---
+++ SuggestionAppLibrary/DataAccess/MongoUserData.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace SuggestionAppLibrary.DataAccess;
+public  class MongoUserData
+{
+	private readonly IMongoCollection<UserModel> _users;
+
+	public MongoUserData(IDbConnection db)
+	{
+		_users = db.UserCollection;
+	}
+}
