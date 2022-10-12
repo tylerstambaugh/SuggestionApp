e5fbb595f9f0812c1e863d5c2281f65c4b137320
Tyler Stambaugh <tyler.stambaugh@gmail.com>
2022-10-12 19:20:27 -0400

    23:06 lesson 11

--- SuggestionAppLibrary/DataAccess/MongoSuggestionData.cs
+++ SuggestionAppLibrary/DataAccess/MongoSuggestionData.cs
@@ -41,5 +41,60 @@ public class MongoSuggestionData
 		return output.Where(x => x.ApprovedForRelease).ToList();
 	}
 
-	//public 
+	public  async Task<SuggestionModel> GetSuggestion(string id)
+	{
+		var results = await _suggestions.FindAsync(s => s.Id == id);
+
+
+		return results.FirstOrDefault();
+	}
+
+	public async Task<List<SuggestionModel>> GetAllSuggestionsWaitingForApproval()
+	{
+		var output = await GetAllSuggestions();
+		return output.Where(x 
+			=> !x.ApprovedForRelease 
+			&& !x.Rejected).ToList();
+	}
+
+	public async Task UpdateSuggestion(SuggestionModel suggestion)
+	{
+		await _suggestions.ReplaceOneAsync(s => s.Id == suggestion.Id, suggestion);
+		_cache.Remove(CacheName);
+	}
+
+	public async Task UpvoteSuggestion(string suggestionId, string userId)
+	{
+		var client = _db.Client;
+
+		using var session = await client.StartSessionAsync();
+
+		session.StartTransaction();
+
+		try
+		{
+			var db = client.GetDatabase(_db.DbName);
+
+			var suggestionsInTransaction = db.GetCollection<SuggestionModel>(_db.SuggestionCollectionName);
+			var suggestion = (await suggestionsInTransaction.FindAsync(s => s.Id == suggestionId)).First();
+			
+			//find out if already an upvote
+			bool isUpvote = suggestion.UserVotes.Add(userId);
+
+			//remove an already upvoted vote
+			if (isUpvote == false)
+			{
+				suggestion.UserVotes.Remove(userId);
+			}
+
+			var userInTransaction = db.GetCollection<UserModel>(_db.UserCollectionName);
+			var user = (await userInTransaction.FindAsync(u => u.Id == userId)).First();
+
+		}
+		catch (Exception ex)
+		{
+			await session.AbortTransactionAsync();
+			throw;
+		}
+	}
 }
